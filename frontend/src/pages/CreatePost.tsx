35ffdcd967b7ce4d68ceafff2263adb49ee0ec1e
nod1xh <Elmedin_Hodzic@outlook.com>
2024-02-06 15:39:48 +0100

    Refactored POST request

--- frontend/src/pages/CreatePost.tsx
+++ frontend/src/pages/CreatePost.tsx
@@ -1,17 +1,51 @@
 import axios from "axios";
+import { useState } from "react";
 
 export default function CreatePost() {
+  const [formData, setFormData] = useState({
+    title: "",
+    image: null as File | null,
+    content: "",
+    author: "",
+    date: "",
+  });
+
+  function handleChange(
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  }
+
+  function handleImageChange(e: React.ChangeEvent<HTMLInputElement>) {
+    if (e.target.files && e.target.files.length > 0) {
+      // If user choose the file or not
+      setFormData({
+        ...formData,
+        image: e.target.files[0],
+      });
+    }
+  }
+
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
-    const formData = new FormData(e.currentTarget);
+    const formDataToSend = new FormData();
+    formDataToSend.append("title", formData.title);
+    formDataToSend.append("image", formData.image as File);
+    formDataToSend.append("content", formData.content);
+    formDataToSend.append("author", formData.author);
+    formDataToSend.append("date", formData.date);
 
     try {
       const response = await axios.post(
         "http://localhost:5000/allposts",
-        formData
+        formDataToSend
       );
-      console.log(response.data.data);
+      console.log(response.data.data, "Post successfully created!");
     } catch (error) {
       console.error("There has been an error creating a post:", error);
     }
@@ -28,11 +62,23 @@ export default function CreatePost() {
       >
         <div>
           <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label htmlFor="image">Image</label>
-          <input type="file" id="image" name="image" />
+          <input
+            type="file"
+            id="image"
+            name="image"
+            accept="image/*"
+            onChange={handleImageChange}
+          />
         </div>
         <div>
           <label htmlFor="content">Content</label>
@@ -41,15 +87,29 @@ export default function CreatePost() {
             name="content"
             rows={10}
             className="border-2 focus:outline-none w-full"
+            value={formData.content}
+            onChange={handleChange}
           ></textarea>
         </div>
         <div>
           <label htmlFor="author">Author</label>
-          <input type="text" id="author" name="author" />
+          <input
+            type="text"
+            id="author"
+            name="author"
+            value={formData.author}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label htmlFor="date">Date</label>
-          <input type="date" id="date" name="date" />
+          <input
+            type="date"
+            id="date"
+            name="date"
+            value={formData.date}
+            onChange={handleChange}
+          />
         </div>
         <button className="p-3 border-2 mt-4 rounded-md bg-slate-500 font-bold">
           Create Post
